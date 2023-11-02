1f1b4f0b7ea02d145264aa791577cf628c5c93b4
Mohit <mohithira555@gmail.com>
2023-11-03 00:34:37 +0530

    Comments

--- app/src/main/java/com/mohit/expensetracker/db.kt
+++ app/src/main/java/com/mohit/expensetracker/db.kt
@@ -5,7 +5,7 @@ import com.mohit.expensetracker.models.Expense
 import com.mohit.expensetracker.models.Income
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
-
+//database
 val config = RealmConfiguration.create(schema = setOf(Expense::class, Category::class, Income::class))
 
 val db: Realm = Realm.open(config)
